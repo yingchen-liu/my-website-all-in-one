14ae64d31dc74610d9762f9d5c595f95ee3eb64d
Yingchen <info@yingchenliu.com>
2024-05-29 11:18:12 +1000

    Change stale time to 24h

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,13 @@ import { router } from "./router";
 import "./reset.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 60 * 24  // 24h
+    }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
