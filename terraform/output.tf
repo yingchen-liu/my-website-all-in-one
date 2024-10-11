cdf774645ae74303057b2a13e29ec07b1ae8d33f
Yingchen <info@yingchenliu.com>
2024-10-11 12:32:08 +0800

    Fix outout

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,9 +3,13 @@ output "route53_name_servers" {
 }
 
 output "ecs_cluster_name" {
-  value = aws_ecs_cluster.your_cluster.name
+  value = aws_ecs_cluster.my_cluster.name
 }
 
-output "ecs_service_name" {
-  value = aws_ecs_service.your_service.name
+output "ecs_service_name_vite_app" {
+  value = aws_ecs_service.web_service.name
+}
+
+output "ecs_service_name_spring_boot_app" {
+  value = aws_ecs_service.spring_boot_service.name
 }
